9745fe8786c7d4373f040872e0fc1df752ba78bf
mahmudh <mahmudhssn246@gmail.com>
2015-10-19 18:04:46 +0100

    maximum top up limit of 90 set

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
   attr_reader :balance
+  DEFAULT_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up amount
+    raise "Maximum limit of #{DEFAULT_LIMIT} exceeded" if balance + amount > DEFAULT_LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,6 +12,11 @@ describe Oystercard do
     it 'can top up the balance' do
       expect{subject.top_up(1)}.to change{subject.balance}.by 1
     end
+
+    it 'raises an error if the balance is over a certain limit when topping up' do
+      subject.top_up(90)
+      expect{subject.top_up(1)}.to raise_error "Maximum limit of #{90} exceeded"
+    end
   end
 
 
